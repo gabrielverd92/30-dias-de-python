fc13c5f9427d4b0308638ea4daa6cea0d6ffd47a
Gabriel Verde <gabriel.verde19ea@cenidet.edu.mx>
2023-03-21 23:41:50 -0600

    Create HolaMundo.py
    Conceptos básicos, operadores sencillos y tipos de datos

---
+++ HolaMundo.py
@@ -0,0 +1,21 @@
+# Introducción
+# Day 1 - 30DiasDePython Challenge
+
+print(2 + 3)   # Adicion(+)
+print(3 - 1)   # subtraccion(-)
+print(2 * 3)   # multiplicacion(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponencial(**)
+print(3 % 2)   # modulo(%)
+print(3 // 2)  # Operador division piso (Floor //)
+
+# Tipos de datos
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Gabriel'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Gabriel'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
